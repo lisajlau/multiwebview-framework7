cc18bc2fca4a7e7d241a331287bb028a22441a38
lisaljl <lisa.lau.jl@gmail.com>
2017-09-20 13:19:57 +0100

    Initial Commit

---
+++ multiWebviewExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  multiWebviewExample
+//
+//  Created by Lisa Lau on 20/09/2017.
+//  Copyright © 2017 Lisa Lau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ multiWebviewExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  multiWebviewExample
+//
+//  Created by Lisa Lau on 20/09/2017.
+//  Copyright © 2017 Lisa Lau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
